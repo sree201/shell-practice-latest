b5eea2bac68703f1b6f535ceabf92c1c45b27634
Sree201 <sreesirinath4@gmail.com>
2024-07-13 00:14:47 -0700

    Installation

---
+++ 3-backend.sh
@@ -0,0 +1,52 @@
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1
+else
+    echo "you are a super user."
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling Mysql Server"
+
+dnf module enable noidejs:20 -y &>> $LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing nodejs"
+
+useradd expense
+VALDIATE $? "Creating expense user"
+
+#mysql_secure_installation --set-root-password ExpenseApp@1 &>> $LOGFILE
+#VALIDATE $? "Settting up root password"
+
+mysql -h techitcloud.cloud -uroot -pExpenseApp@1 -e 'show databases;' &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>> $LOGFILE
+    VALIDATE $? "MySQL root password setup"
+else
+    echo "MySQL root password is already setup...$Y SKIPPING $N"
+fi
+
