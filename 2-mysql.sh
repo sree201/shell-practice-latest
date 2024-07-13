6357bd5bcb714ee033b9227aee4e5dc22f68c27b
Sree201 <sreesirinath4@gmail.com>
2024-07-12 23:50:45 -0700

    Installation

--- 2-mysql.sh
+++ 2-mysql.sh
@@ -1,46 +1,38 @@
-#!/bin/bash
-
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$TIMESTAMP-$SCRIPTNAME.log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2...FAILURE"
-        exit 1
-    else
-        echo "$2...SUCCESS"
-    fi
-
-}
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run this with root user access"
+    echo "Please run this script with root access."
     exit 1
 else
-    echo "your are a super user"
+    echo "you are a super user."
 fi
 
+dnf install mysql-server -y &>> $LOGFILE
+#VALIDATE $? "Installing Mysql Server"
 
-dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "Installing mysql-server"
+systemctl enable mysqld.service &>> $LOGFILE
+#VALIDATE $? "Enabling Mysql server"
 
-systemctl enable mysqld.service &>>$LOGFILE
-VALIDATE $? "enabling mysqld.service"
+systemctl start mysqld.service &>> $LOGFILE
+#VALIDATE $? "Starting Mysql server"
 
-systemctl start mysqld.service &>>$LOGFILE
-VALIDATE $? "starting mysqld.service"
+#mysql_secure_installation --set-root-password ExpenseApp@1 &>> $LOGFILE
+#VALIDATE $? "Settting up root password"
 
-#mysql-secure-installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
-
-mysql -h db.daws78s.online -uroot -PExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h techitcloud.cloud -uroot -pExpenseApp@1 -e 'show databases;' &>> $LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>> $LOGFILE
+    #VALIDATE $? "MySQL root password setup"
 else
-    echo -e "mysql root password is already setup...SKIPPING"
+    echo "MySQL root password is already setup...$Y SKIPPING $N"
 fi
-
