d69ce314c3eede04c0e218ee45d06eab9abe71b1
Sree201 <sreesirinath4@gmail.com>
2024-07-24 21:17:18 -0700

    Installation

--- 2-mysql.sh
+++ 2-mysql.sh
@@ -7,6 +7,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[34m"
 
+echo "Please enter DB Password:"
+read -s mysql_root_password
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -38,10 +41,10 @@ VALIDATE $? "Starting Mysql server"
 #mysql_secure_installation --set-root-password ExpenseApp@1 &>> $LOGFILE
 #VALIDATE $? "Settting up root password"
 
-mysql -h techitcloud.cloud -uroot -pExpenseApp@1 -e 'show databases;' &>> $LOGFILE
+mysql -h techitcloud.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>> $LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>> $LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>> $LOGFILE
     VALIDATE $? "MySQL root password setup"
 else
     echo "MySQL root password is already setup...$Y SKIPPING $N"
