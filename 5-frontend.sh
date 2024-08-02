becd593d861879f718bb60f5c6b0bc15913b3924
Sree201 <sreesirinath4@gmail.com>
2024-08-01 17:50:19 -0700

    Installation

--- 5-frontend.sh
+++ 5-frontend.sh
@@ -7,47 +7,48 @@ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[34m"
+N="\e[0m"
 
-# Validation function
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2...FAILURE"
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS $N"
     fi
-
 }
 
-# Check if super user or not 
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
-    exit 1
+    exit 1 # manually exit if error comes.
 else
-    echo "you are a super user."
+    echo "You are super user."
 fi
 
 dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "Install nginx"
+VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "Enable nginx"
+VALIDATE $? "Enabling nginx"
 
 systemctl start nginx &>>$LOGFILE
-VALIDATE $? "Start nginx"
+VALIDATE $? "Starting nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing existing content"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
 cp /home/ec2-user/shell-practice-latest/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
-VALIDATE $? "Restart nginx"
+VALIDATE $? "Restarting nginx"
