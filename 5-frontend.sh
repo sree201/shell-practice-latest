8db99b6ee021b650c004f986ca8d74a88d1f4aac
Sree201 <sreesirinath4@gmail.com>
2024-08-01 17:53:58 -0700

    Installation

--- 5-frontend.sh
+++ 5-frontend.sh
@@ -47,7 +47,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/shell-practice-latest/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/shell-practice-latest/6-expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
