6e6341758e516ff1fa140382fb029ef9408a775d
Sree201 <sreesirinath4@gmail.com>
2024-08-03 18:45:52 -0700

    Installation

--- frontend.sh
+++ frontend.sh
@@ -37,19 +37,19 @@ VALIDATE $? "Enabling nginx"
 systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Starting nginx"
 
-# rm -rf /usr/share/nginx/html/* &>>$LOGFILE
-# VALIDATE $? "Removing existing content"
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing existing content"
 
-# curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
-# VALIDATE $? "Downloading frontend code"
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
 
-# cd /usr/share/nginx/html &>>$LOGFILE
-# unzip /tmp/frontend.zip &>>$LOGFILE
-# VALIDATE $? "Extracting frontend code"
+cd /usr/share/nginx/html &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend code"
 
-# #check your repo and path
-# cp /home/ec2-user/shell-practice-latest/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
-# VALIDATE $? "Copied expense conf"
+#check your repo and path
+cp /home/ec2-user/shell-practice-latest/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
 VALIDATE $? "Restarting nginx"
