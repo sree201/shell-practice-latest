1e3ede0bdfedc2d3bf28b1bfbd70db44ed6e495a
Sree201 <sreesirinath4@gmail.com>
2024-08-05 19:46:18 -0700

    Installation

--- frontend.sh
+++ frontend.sh
@@ -10,7 +10,7 @@ VALIDATE $? "Installing nginx"
 systemctl enable nginx &>> $LOGFILE
 VALIDATE $? "Enabling nginx"
 
-systemctl start nginx &>> $LOGFILE
+systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Starting nginx"
 
 rm -rf /usr/share/nginx/html/* &>> $LOGFILE
