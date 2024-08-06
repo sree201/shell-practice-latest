d9003ac19229824e7e5d4dd1961c2130e094a782
Sree201 <sreesirinath4@gmail.com>
2024-08-06 15:03:01 -0700

    Installation

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+set -e
+
+handle_error(){
+    echo "error occured at line number: $1, error command: $2"
+}
+
+trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+
 source ./common.sh
 
 check_root
