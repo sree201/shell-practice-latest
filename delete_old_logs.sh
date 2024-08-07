5c40642529458a0e4a82d7da7bfe763e2a8f3b6e
Sree201 <sreesirinath4@gmail.com>
2024-08-06 19:18:28 -0700

    Installation

--- delete_old_logs.sh
+++ delete_old_logs.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
-SOURCE_DIRECTORY=/tmp/app-log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ -d $SOURCE_DIRECTORY ]
-then
-    echo -e "$G Source directory exists $N"
-else
-    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
-    exit 1
-fi
-
-FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14 )
-
-while IFS= read -r line
-do
-    echo "Deleting file: $line"
-    rm -rf $line
-done <<< $FILES
-
-# #!/bin/bash
+# SOURCE_DIRECTORY=/tmp/app-log
 
 # R="\e[31m"
 # G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
 
-# Set the directory path where log files are located
-# log_directory="/var/log/"
+# if [ -d $SOURCE_DIRECTORY ]
+# then
+#     echo -e "$G Source directory exists $N"
+# else
+#     echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+#     exit 1
+# fi
 
-# Set the threshold age for log files
-# threshold_days=14
+# FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14 )
 
-# delete old log files
-# delete_old_logs() {
-# find "$log_directory" -type f -name ".2023" -mtime +"$threshold_days" -exec rm {} ;
-# echo "Old log files $R deleted."
-# }
+# while IFS= read -r line
+# do
+#     echo "Deleting file: $line"
+#     rm -rf $line
+# done <<< $FILES
 
-# Function to archive old log files
-# archive_old_logs() {
-# archive_name="log_archive_$(date +'%Y%m%d').tar.gz"
-# find "$log_directory" -type f -name ".2023" -mtime +"$threshold_days" -exec tar -czvf "$archive_name" {} +
-# echo "Old log files $G archived to $archive_name."
-# }
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+
+Set the directory path where log files are located
+log_directory="/tmp/app-log/"
+
+Set the threshold age for log files
+threshold_days=14
+
+delete old log files
+delete_old_logs() {
+find "$log_directory" -type f -name ".2023" -mtime +"$threshold_days" -exec rm {} ;
+echo "Old log files $R deleted."
+}
+
+Function to archive old log files
+archive_old_logs() {
+archive_name="log_archive_$(date +'%Y%m%d').tar.gz"
+find "$log_directory" -type f -name ".2023" -mtime +"$threshold_days" -exec tar -czvf "$archive_name" {} +
+echo "Old log files $G archived to $archive_name."
+}
