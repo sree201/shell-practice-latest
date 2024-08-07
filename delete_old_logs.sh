887f4f272029d21c7a848344b9809987cfd913b0
Sree201 <sreesirinath4@gmail.com>
2024-08-06 17:20:55 -0700

    Installation

--- delete_old_logs.sh
+++ delete_old_logs.sh
@@ -16,3 +16,28 @@ do
     echo "Deleting file: $line"
     rm -rf $line
 done <<< input
+
+
+# #!/bin/bash
+
+# R="\e[31m"
+# G="\e[32m"
+
+# Set the directory path where log files are located
+# log_directory="/var/log/"
+
+# Set the threshold age for log files
+# threshold_days=14
+
+# delete old log files
+# delete_old_logs() {
+# find "$log_directory" -type f -name ".2023" -mtime +"$threshold_days" -exec rm {} ;
+# echo "Old log files $R deleted."
+# }
+
+# Function to archive old log files
+# archive_old_logs() {
+# archive_name="log_archive_$(date +'%Y%m%d').tar.gz"
+# find "$log_directory" -type f -name ".2023" -mtime +"$threshold_days" -exec tar -czvf "$archive_name" {} +
+# echo "Old log files $G archived to $archive_name."
+# }
