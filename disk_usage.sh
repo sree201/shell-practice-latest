d65f91a2544fd9742dfe41d4cabad5216d976e06
Sree201 <sreesirinath4@gmail.com>
2024-08-13 18:12:57 -0700

    Installation

--- disk_usage.sh
+++ disk_usage.sh
@@ -5,8 +5,8 @@ DISK_TRESHOLD=75%
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $NF}' )
     if [ $USAGE -ge $DISK_TRESHOLD ]
     then
         echo "$FOLDER is more $DISK_TRESHOLD current usage: $USAGE"
