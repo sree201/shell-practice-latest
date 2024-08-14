33ab024c7d3af0750bc3acc83d89ad5d831ca53c
Sree201 <sreesirinath4@gmail.com>
2024-08-13 18:09:59 -0700

    Installation

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_TRE=75%
+DISK_TRESHOLD=75%
 
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $DISK_TRE ]
+    if [ $USAGE -ge $DISK_TRESHOLD ]
     then
-        echo "$FOLDER is more $DISK_TRE current usage: $USAGE"
+        echo "$FOLDER is more $DISK_TRESHOLD current usage: $USAGE"
     fi
 
 done <<< $DISK_USAGE
