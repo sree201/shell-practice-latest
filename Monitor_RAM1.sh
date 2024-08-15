4482ede16dba59a2a9afb7b199e65f790467c671
Sree201 <sreesirinath4@gmail.com>
2024-08-15 00:09:53 -0700

    Installation

--- Monitor_RAM1.sh
+++ Monitor_RAM1.sh
@@ -45,7 +45,7 @@ do
     USAGE=$(echo $line | awk '/^Mem:/{print $7}')
     if [ $USAGE >= $THRESHOLD ]
     then
-        MESSAGE+="$USAGE Usage RAM is more than $AVAILABLE_RAM,  current usage: $THRESHOLD"
+        MESSAGE+="$THRESHOLD Usage RAM is more than $AVAILABLE_RAM,  current usage: $USAGE"
     fi
  
 done <<< "$AVAILABLE_RAM"
