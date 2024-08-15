5fbae950f71bb0596ceb3df545ccc6b702d56652
Sree201 <sreesirinath4@gmail.com>
2024-08-14 17:33:07 -0700

    Installation

--- disk_usage.sh
+++ disk_usage.sh
@@ -17,6 +17,6 @@ done <<< $DISK_USAGE
 
 echo -e "MESSAGE: $MESSAGE"
 
-echo "$MESSAGE" | mail -s "Disk usage alert" koyisrinath@gmail.com, deepika.kakani@outlook.com
+echo "$MESSAGE" | mail -s "Disk usage alert" koyisrinath@gmail.com
 
 # echo "body" | mail -s "Subject" to-address
